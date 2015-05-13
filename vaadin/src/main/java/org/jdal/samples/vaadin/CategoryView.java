361bc55574778283d051ccc922ba29d766c39752
Jose Luis Martin <jlm@joseluismartin.info>
2015-05-13 15:01:32 +0200

    Fix CategoryView temporal test.

--- vaadin/src/main/java/org/jdal/samples/vaadin/CategoryView.java
+++ vaadin/src/main/java/org/jdal/samples/vaadin/CategoryView.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.jdal.samples.model.Category;
 import org.jdal.vaadin.ui.AbstractView;
 import org.jdal.vaadin.ui.FormUtils;
+import org.jdal.vaadin.ui.form.BoxFormBuilder;
 
 import com.vaadin.ui.Component;
 import com.vaadin.ui.TextField;
@@ -28,14 +29,13 @@ public class CategoryView extends AbstractView<Category> {
 	
 	@Override
 	protected Component buildPanel() {
-		throw new org.springframework.security.access.AccessDeniedException("No se puede");
-//		BoxFormBuilder fb = new BoxFormBuilder();
-//		fb.setDefaultWidth(BoxFormBuilder.SIZE_FULL);
-//		fb.setMargin(false);
-//		fb.row();
-//		fb.add(name, getMessage("categoryName"));
-//		
-//		return fb.getForm();
+		BoxFormBuilder fb = new BoxFormBuilder();
+		fb.setDefaultWidth(BoxFormBuilder.SIZE_FULL);
+		fb.setMargin(false);
+		fb.row();
+		fb.add(name, getMessage("categoryName"));
+		
+		return fb.getForm();
 	}
 
 }
